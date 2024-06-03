1669af17e1fe9d2e94f6dd61e5b5330fdbb5c750
Shaunicator <65745081+Shaunicator@users.noreply.github.com>
2024-06-03 18:33:43 +1200

    Fixed missing images. Added more data and more styles to POC

--- public/dom.js
+++ public/dom.js
@@ -52,6 +52,26 @@ function getData(url,){
     document.getElementById('upk-rare').innerText = dataObject.unitData[unitType][level]["Cost"]["Upkeep"]["Rare"];
     document.getElementById('upk-mp').innerText = dataObject.unitData[unitType][level]["Cost"]["Upkeep"]["Manpower"];
     document.getElementById('upk-money').innerText = dataObject.unitData[unitType][level]["Cost"]["Upkeep"]["Money"];
+
+    document.getElementById('prod-time').innerText = dataObject.unitData[unitType][level]["Cost"]["Production"]["Time"];
+
+    document.getElementById('atk-una').innerText = dataObject.unitData[unitType][level]["Strength"]["Attack"]["Unarmored"];
+    document.getElementById('atk-light').innerText = dataObject.unitData[unitType][level]["Strength"]["Attack"]["Light"];
+    document.getElementById('atk-heavy').innerText = dataObject.unitData[unitType][level]["Strength"]["Attack"]["Heavy"];
+    document.getElementById('atk-air').innerText = dataObject.unitData[unitType][level]["Strength"]["Attack"]["Air"];
+    document.getElementById('atk-sea').innerText = dataObject.unitData[unitType][level]["Strength"]["Attack"]["Ship"];
+    document.getElementById('atk-sub').innerText = dataObject.unitData[unitType][level]["Strength"]["Attack"]["Sub"];
+    document.getElementById('atk-build').innerText = dataObject.unitData[unitType][level]["Strength"]["Attack"]["Building"];
+    document.getElementById('atk-morale').innerText = dataObject.unitData[unitType][level]["Strength"]["Attack"]["Morale"];
+
+    document.getElementById('def-una').innerText = dataObject.unitData[unitType][level]["Strength"]["Defense"]["Unarmored"];
+    document.getElementById('def-light').innerText = dataObject.unitData[unitType][level]["Strength"]["Defense"]["Light"];
+    document.getElementById('def-heavy').innerText = dataObject.unitData[unitType][level]["Strength"]["Defense"]["Heavy"];
+    document.getElementById('def-air').innerText = dataObject.unitData[unitType][level]["Strength"]["Defense"]["Air"];
+    document.getElementById('def-sea').innerText = dataObject.unitData[unitType][level]["Strength"]["Defense"]["Ship"];
+    document.getElementById('def-sub').innerText = dataObject.unitData[unitType][level]["Strength"]["Defense"]["Sub"];
+    document.getElementById('def-build').innerText = dataObject.unitData[unitType][level]["Strength"]["Defense"]["Building"];
+    
   })
 }
 
